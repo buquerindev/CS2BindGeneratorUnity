3e597fb12a003f5287b1b51481f50af59737b500
S0DI0 <pbalejandro01@gmail.com>
2025-06-21 02:50:58 +0100

    Ascii Art

--- Assets/Scripts/Binds/BindManager.cs
+++ Assets/Scripts/Binds/BindManager.cs
@@ -25,6 +25,48 @@ public class BindManager : MonoBehaviour
     [SerializeField] private Button hiddenButton;
     [SerializeField] private Button exportBindsButton;
 
+    private string[] csLogoASCII = new string[]
+    {
+        @"                                                   $$$$$                                          ",
+        @"                                                  X$$$$$$                                         ",
+        @"                                                 .:$$$$$............                              ",
+        @"                                               X$$$$$$$$$$$$$$$$$$$.                              ",
+        @"                                              X$$$$$$$$$$$$$$                                     ",
+        @"                                            .;$$$$$$$$$$$$$$$                                     ",
+        @"             .$$$$$$$$$$$$$$$$$$$$$$$X      $$$$$$$$$$X$$$           $$$$$$$$$$$$$$$$$$$$$$$$$:   ",
+        @"            $$$$$$$$$$$$$$$$$$$$$$$$$      $$$$$$$$$$$             $$$$$$$$$$$$$$$$$$$$$$$$$$$    ",
+        @"           $$$$$$$$$$$$$$$$$$$$$$$$X        $$$$$$$$$             $$$$$$$$$$$$$$$$$$$$$$$$$$;     ",
+        @"          $$$$$$$                          $$$$$$$$x             $$$$$$$                          ",
+        @"         $$$$$$$                          $$$$$$$$$$            X$$$$$$                           ",
+        @"        x$$$$$$.                         $$$$$$$$$$$$           $$$$$$$$$$$$$$$$$$$$$$$$          ",
+        @"        x$$$$$$.                         $$$$$$$$$$$$           $$$$$$$$$$$$$$$$$$$$$$$$          ",
+        @"       $$$$$$$                          X$$$$x   $$$$$$           ;$$$$$$$$$$$$$$$$$$$$$$         ",
+        @"      $$$$$$$                          $$$$$$      $$$$+                         .$$$$$$          ",
+        @"     $$$$$$$                          $$$$$$      $$$$$                          $$$$$$;          ",
+        @"     $$$$$$$$$$$$$$$$$$$$$$$$$       $$$$$:      $$$$$      $$$$$$$$$$$$$$$$$$$$$$$$$$X           ",
+        @"     ;$$$$$$$$$$$$$$$$$$$$$$$       $$$$$        $$$$.     $$$$$$$$$$$$$$$$$$$$$$$$$$.            ",
+        @"       +$$$$$$$$$$$$$$$$$$$$       X$$$.        +$$$x     $$$$$$$$$$$$$$$$$$$$$$$$.               ",
+        @"                                   $$$          $$$+                                              ",
+        @"                                  $$$$         :$$$$$$                                            ",
+        @"                ____  _           __  ______                           __                         ",
+        @"               / __ )(_)___  ____/ / / ____/__  ____  ___  _________ _/ /_____  _____             ",
+        @"              / __  / / __ \/ __  / / / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/             ",
+        @"             / /_/ / / / / / /_/ / / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /                 ",
+        @"            /_____/_/_/ /_/\__,_/  \____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/                  ",
+        @"                                                                                                  ",
+        @"                                            Created by:                                           ",
+        @"                                           @buquerindev                                           ",
+        @"              . -------------------------------------------------------------------.              ",
+        @"              | [Esc] [F1][F2][F3][F4][F5][F6][F7][F8][F9][F0][F10][F11][F12] o o o|              ",
+        @"              |                                                                    |              ",
+        @"              | [`][1][2][3][4][5][6][7][8][9][0][-][=][_<_] [I][H][U] [N][/][*][-]|              ",
+        @"              | [|-][Q][W][E][R][T][Y][U][I][O][P][{][}] | | [D][E][D] [7][8][9]|+||              ",
+        @"              | [CAP][A][S][D][F][G][H][J][K][L][;]['][#]|_|           [4][5][6]|_||              ",
+        @"              | [^][\][Z][X][C][V][B][N][M][,][.][/] [__^__]    [^]    [1][2][3]| ||              ",
+        @"              | [c]   [a][________________________][a]   [c] [<][V][>] [ 0  ][.]|_||              ",
+        @"              `--------------------------------------------------------------------'              "
+    };
+
     private Dictionary<string, Transform> categoryContainers;
 
     private void Start()
@@ -111,6 +153,8 @@ public class BindManager : MonoBehaviour
 
     private void ExportBinds()
     {
+        
+
         // Create a .cfg file
         string filePath = Path.Combine(Application.dataPath, "binds.cfg");
 
@@ -128,18 +172,23 @@ public class BindManager : MonoBehaviour
                     .ToDictionary(subgroup => subgroup.Key, subgroup => subgroup.ToList())
             );
 
-
         //CommandList audioCommands = (CommandList)commandList.commands.GroupBy(cmd => cmd.category);
 
         using (StreamWriter writer = new StreamWriter(filePath))
         {
+
+            foreach (string line in csLogoASCII)
+            {
+                writer.Write($"echo \"{line}\"\n");
+            }
+
             foreach (var categoryPair in groupedBinds)
             {
                 var subcategories = categoryPair.Value;
                 foreach (var subcategory in subcategories)
                 {
                     writer.WriteLine();
-                    writer.WriteLine($"========== {categoryPair.Key.ToUpper()} - {subcategory.Key} ==========");
+                    writer.WriteLine($"//========== {categoryPair.Key.ToUpper()} - {subcategory.Key} ==========");
 
                     var binds = subcategory.Value;
                     foreach (var bind in binds)
